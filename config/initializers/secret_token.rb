17b1f0c06066f55b2e20b058f58012ba5d078d1c
Hironori Onoue <onoue@Hironori-no-MacBook-Air.local>
2012-04-03 21:15:13 +0900

    delete

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Meimeisan::Application.config.secret_token = '46540af4ce858ab3240d0cd53e27170de734981386f98f5ede7193213ef479dfe16c092ad293ff58afa00011101fe4fd29027f8239ad0db20b617465d1e32ca5'
